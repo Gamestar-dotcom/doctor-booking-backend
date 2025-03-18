7a3b4ca8d48d67240f91fbb5cfe1335b5d8ef4d7
dotcom <gamestaraficionado@gmail.com>
2025-03-18 06:28:42 +0300

    comment cors

--- server.js
+++ server.js
@@ -19,12 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "https://gamestar-dotcom.github.io",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+  cors()
+  //   {
+  //   origin: "https://gamestar-dotcom.github.io",
+  //   methods: ["GET", "POST", "PUT", "DELETE"],
+  //   allowedHeaders: ["Content-Type", "Authorization"],
+  //   credentials: true,
+  // }
 );
 
 // Routes
